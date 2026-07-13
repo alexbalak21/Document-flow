f3daaa8706aed3498f0b021001aaf6181a148930
alexbalak21 <alex.balak@outlook.com>
2026-07-13 14:05:18 +0200

    fix paginator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,9 @@
 <?php
 
+
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,11 +16,9 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
+
     public function boot(): void
     {
-        //
+        Paginator::useBootstrapFive();
     }
 }
