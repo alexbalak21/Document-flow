d3ff4808eff35f382833c57acb85e33aa1ef5c06
alexbalak21 <alex.balak@outlook.com>
2026-07-16 11:33:19 +0200

    removed the unnecessary packages

--- config/services.php
+++ config/services.php
@@ -34,5 +34,8 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
-
+    
+    'pdf' => [
+        'url' => env('PDF_SERVICE_URL', 'http://127.0.0.1:5001'),
+    ],
 ];
